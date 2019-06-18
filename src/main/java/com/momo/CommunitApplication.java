5277609797c8b32fc58ed86fc3fcdff968d482af
root <347471264@qq.com>
2019-06-18 15:36:31 +0800

    完成boot框架搭建

---
+++ src/main/java/com/momo/CommunitApplication.java
@@ -0,0 +1,13 @@
+package com.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitApplication.class, args);
+    }
+
+}

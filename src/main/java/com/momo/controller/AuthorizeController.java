ae0ba23466b3b0fb52652be79c37845bff94745e
root <347471264@qq.com>
2019-06-20 10:53:21 +0800

    完成github三方登陆

--- src/main/java/com/momo/controller/AuthorizeController.java
+++ src/main/java/com/momo/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.momo.controller;
 
 import com.momo.dto.AccessTokenDto;
 import com.momo.dto.GithubUser;
+import com.momo.mapper.UserMapper;
+import com.momo.model.User;
 import com.momo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,8 +27,11 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setCode(code);
         accessTokenDto.setRedirect_uri(redirectUri);
@@ -31,8 +39,21 @@ public class AuthorizeController {
         accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user);
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，存入session
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            //登陆成功，重新登陆
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/momo/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.momo.mapper;
+
+import com.momo.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    //默认修饰符为public 所以可以去掉
+    @Insert("insert into user (name,ACCOUNT_ID,TOKEN,GMT_CREATE,GMT_MODIFIED) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
